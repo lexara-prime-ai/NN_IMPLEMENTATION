a319ce77d76a4eef08f751d00135374edad4168b
Irfan Ghat <irfanghat@gmail.com>
2024-01-09 18:30:18 +0000

    Increased accuracy

--- src/network.rs
+++ src/network.rs
@@ -47,14 +47,11 @@ impl Network<'_> {
         }
     }
 
-    // Todo -> Read on activation functions e.g Sigmoid function :: 1/(1 + e^-x)
     pub fn feed_forward(&mut self, inputs: Vec<f64>) -> Vec<f64> {
-        // If we don't get the right amount of inputs
         if inputs.len() != self.layers[0] {
-            panic!("Invalid no. of inputs");
+            panic!("Invalid inputs length");
         }
 
-        // current -> Keeps track of the current layer
         let mut current = Matrix::from(vec![inputs]).transpose();
         self.data = vec![current.clone()];
 
@@ -69,28 +66,21 @@ impl Network<'_> {
         current.transpose().data[0].to_owned()
     }
 
-    // Back propagation
-    // Find errors and use the gradient matrix to find out what went wrong
-    // and tweak the weights to get a better result
-    pub fn back_propagate(&mut self, outputs: Vec<f64>, targets: Vec<f64>) {
+    pub fn back_propogate(&mut self, outputs: Vec<f64>, targets: Vec<f64>) {
         if targets.len() != self.layers[self.layers.len() - 1] {
-            panic!("Invalida no. of targets");
+            panic!("Invalid targets length");
         }
 
         let parsed = Matrix::from(vec![outputs]).transpose();
-        // Get difference between each individual output neuron
         let mut errors = Matrix::from(vec![targets]).transpose().subtract(&parsed);
-        // Switch from function to derivative -> Why??
         let mut gradients = parsed.map(self.activation.derivative);
 
-        // Go backwards
         for i in (0..self.layers.len() - 1).rev() {
             gradients = gradients
                 .dot_multiply(&errors)
                 .map(&|x| x * self.learning_rate);
 
             self.weights[i] = self.weights[i].add(&gradients.multiply(&self.data[i].transpose()));
-            // Subtract...add?
             self.biases[i] = self.biases[i].add(&gradients);
 
             errors = self.weights[i].transpose().multiply(&errors);
@@ -98,41 +88,37 @@ impl Network<'_> {
         }
     }
 
-    // epochs -> The number of times we want to cycle through the targets
     pub fn train(&mut self, inputs: Vec<Vec<f64>>, targets: Vec<Vec<f64>>, epochs: u16) {
-        println!("\n * * * * MODEL STATUS * * * *\n");
-
         for i in 1..=epochs {
             if epochs < 100 || i % (epochs / 100) == 0 {
                 println!("Epoch {} of {}", i, epochs);
             }
-        }
-
-        for j in 0..inputs.len() {
-            let outputs = self.feed_forward(inputs[j].clone());
-            self.back_propagate(outputs, targets[j].clone());
+            for j in 0..inputs.len() {
+                let outputs = self.feed_forward(inputs[j].clone());
+                self.back_propogate(outputs, targets[j].clone());
+            }
         }
     }
 
     pub fn save(&self, file: String) {
-        let mut file = File::create(file).expect("Unable to touch save file...");
+        let mut file = File::create(file).expect("Unable to touch save file");
 
         file.write_all(
-            json!({
-                "weights": self.weights.clone().into_iter().map(|matrix| matrix.data).collect::<Vec<Vec<Vec<f64>>>>(),
-                "biases": self.biases.clone().into_iter().map(|matrix| matrix.data).collect::<Vec<Vec<Vec<f64>>>>()
-            }).to_string().as_bytes()
-        ).expect("Unable to write to save file...");
+			json!({
+				"weights": self.weights.clone().into_iter().map(|matrix| matrix.data).collect::<Vec<Vec<Vec<f64>>>>(),
+				"biases": self.biases.clone().into_iter().map(|matrix| matrix.data).collect::<Vec<Vec<Vec<f64>>>>()
+			}).to_string().as_bytes(),
+		).expect("Unable to write to save file");
     }
 
     pub fn load(&mut self, file: String) {
-        let mut file = File::open(file).expect("Unable to open save file...");
+        let mut file = File::open(file).expect("Unable to open save file");
         let mut buffer = String::new();
 
         file.read_to_string(&mut buffer)
-            .expect("Unable to read save file...");
+            .expect("Unable to read save file");
 
-        let save_data: SaveData = from_str(&buffer).expect("Unable to serialize save data...");
+        let save_data: SaveData = from_str(&buffer).expect("Unable to serialize save data");
 
         let mut weights = vec![];
         let mut biases = vec![];
